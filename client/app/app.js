1515d29c3db1b3a047480131626f4821794b3f98
leekangtaqi <leekangtaqi@sina.com>
2016-07-27 14:03:56 +0800

    test ok

--- client/app/app.js
+++ client/app/app.js
@@ -26,7 +26,6 @@ if (auth_code) {
   req.send(null);
   token = JSON.parse(req.responseText).token;
 }
-console.warn('22')
 
 var hostname = window.location.hostname.split('.');
 var env = hostname[0];
